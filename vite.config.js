a376b2301e3fb649e61f4e03434de33dd3abe085
shivapareek <pareekshiva2004@gmail.com>
2025-06-22 14:01:06 +0530

    index file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+    base: '/shivaportfolio/', // 👈 this must match your repo name!
 })
